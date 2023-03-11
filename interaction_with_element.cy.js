a5a5d71edc0642d3efcce0ba1b91e8ebe6782930
naufalammar21 <62551010+naufalammar21@users.noreply.github.com>
2023-03-11 21:19:21 +0700

    Update interaction_with_element.cy.js

--- interaction_with_element.cy.js
+++ interaction_with_element.cy.js
@@ -4,22 +4,11 @@ describe('working with inputs',()=>{
     it('Visit the website',()=>{
         cy.visit('http://zero.webappsecurity.com/login.html',{timeout:10000})
         cy.url().should('include','login.html')
-    });
-
-
-    it('Should fill username',()=>{
         cy.get('#user_login').clear()
         cy.get('#user_login').type('username')
-    });
-
-    it('Should fill password',()=>{
         cy.get('#user_password').clear()
         cy.get('#user_password').type('password')
+        // cy.get('#user_remember_me')
+        cy.get('#user_remember_me').check()
     });
-
-    it('Should active checkbox keep me sign in',()=>{
-        cy.get('#user_remember_me')
-        cy.get('#user_password').type('checkbox')
-    });
-
 });
