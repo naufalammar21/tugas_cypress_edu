52fe76c678a2d49ce9b50467436bfe597c60eb91
naufalammar21 <ammarhtr@gmail.com>
2023-03-25 16:08:08 +0700

    update task

--- chapter_6/searchbox_task.cy.js
+++ chapter_6/searchbox_task.cy.js
@@ -7,10 +7,16 @@ describe('Searchbox Test',()=>{
        
     });
     it('should type into searchbox and submit',()=>{
-        cy.get('#searchTerm').type('Zero - Free Access to Online Banking {enter}','a')
-        cy.get('#searchTerm').type('Zero - Online Statements {enter}','a')
-        cy.get('h2').contains('Search Results:')
+        // cy.get('#searchTerm').type('Zero - Free Access to Online Banking {enter}','a')
+        // cy.get('#searchTerm').type('Zero - Online Statements {enter}','a')
+        cy.get('#searchTerm').type('Zero - Account {enter}')
+        // cy.get('h2').contains('Search Results:')
+
+        let checkString = () =>{
+            if(cy.get('.top_offset >ul > li')){
+                cy.get('.top_offset > ul > li ').contains('Zero')
+            }
+        }
+        checkString()
     });
-
-
 });
