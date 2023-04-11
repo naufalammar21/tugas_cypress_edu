daf98d3c29eb55f49cd1fd1a1654c4f0471dadaa
naufalammar21 <ammarhtr@gmail.com>
2023-04-11 22:55:01 +0700

    new task

--- chapter_9/task_validation_content.cy.js
+++ chapter_9/task_validation_content.cy.js
@@ -1,20 +1,37 @@
 /// <reference types="cypress" />
 
-describe("Test Object Ability", () => {
-    it("should have name: limber", () => {
-      const obj = {
-        "abilities": [
-          {
-            "ability": {
-              "name": "limber",
-              "url": "https://pokeapi.co/api/v2/ability/7/"
-            }
-          }
-        ]
-      };
+// describe("Test Object Ability", () => {
+//     it("should have name: limber", () => {
+//       const obj = {
+//         "abilities": [
+//           {
+//             "ability": {
+//               "name": "limber",
+//               "url": "https://pokeapi.co/api/v2/ability/7/"
+//             }
+//           }
+//         ]
+//       };
       
-      // Memastikan objek ability memiliki atribut "name" yang diharapkan
-      expect(obj.abilities[0].ability.name).to.eq("limber");
-    });
-  });
+//       // Memastikan objek ability memiliki atribut "name" yang diharapkan
+//       expect(obj.abilities[0].ability.name).to.eq("limber");
+//     });
+//   });
   
+describe('API Validation Content',()=>{
+  it('Update User',()=>{
+      var user = {
+          "abilities" : [
+              {
+                  "ability":{
+                      "name" : "limber",
+                      "url" : 'https://pokeapi.co/api/v2/ability/7/'
+                  },
+              }
+          ],
+     }
+     cy.request('GET','https://pokeapi.co/api/v2/ability/7/', user).then((response) => {
+      expect(response.body.name).to.eq(user.abilities[0].ability.name)
+     })
+  });
+})
