e80b6eefe8ba2027d62e13090c72b344fb5421a1
naufalammar21 <ammarhtr@gmail.com>
2023-04-11 22:57:14 +0700

    update

--- chapter_9/task_validation_content.cy.js
+++ chapter_9/task_validation_content.cy.js
@@ -1,23 +1,5 @@
 /// <reference types="cypress" />
 
-// describe("Test Object Ability", () => {
-//     it("should have name: limber", () => {
-//       const obj = {
-//         "abilities": [
-//           {
-//             "ability": {
-//               "name": "limber",
-//               "url": "https://pokeapi.co/api/v2/ability/7/"
-//             }
-//           }
-//         ]
-//       };
-      
-//       // Memastikan objek ability memiliki atribut "name" yang diharapkan
-//       expect(obj.abilities[0].ability.name).to.eq("limber");
-//     });
-//   });
-  
 describe('API Validation Content',()=>{
   it('Update User',()=>{
       var user = {
